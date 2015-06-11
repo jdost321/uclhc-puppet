98a0651389cc3cc67db3a8677d88abba48b3ec76
Jeff Dost <jdost@ucsd.edu>
2015-06-11 14:32:12 -0700

    add epel to osg_repos

--- development/osg_repos/manifests/init.pp
+++ development/osg_repos/manifests/init.pp
@@ -1,2 +1,7 @@
-class osg-repos {
+class osg_repos {
+  package {"epel-release":
+    ensure => "installed",
+    provider => "rpm",
+    source => "http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm"
+  }
 }
