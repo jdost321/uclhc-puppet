5edfebdef7e61acd6198be8bc2a8ad9a73180e44
Jeff Dost <jdost@ucsd.edu>
2015-07-28 15:26:35 -0700

    add condor configs for local jobs and flocking to gwms

--- development/condor/manifests/init.pp
+++ development/condor/manifests/init.pp
@@ -12,9 +12,24 @@ class condor ($condor_version = 'latest', $gwms_version = 'latest') {
     install_options => [{'--enablerepo' => 'epel'}, {'--enablerepo' => 'osg'}]
   }
 
+  file {'/etc/condor/certs/condor_mapfile':
+    ensure => 'present',
+    source => 'puppet:///modules/condor/condor_mapfile',
+    require => Package['condor']
+  }
+  file {'/etc/condor/config.d/90_gwms_dns.config':
+    ensure => 'present',
+    source => 'puppet:///modules/condor/90_gwms_dns.config',
+    require => Package['condor']
+  }
   file {'/etc/condor/config.d/91_single_host.config':
     ensure => 'present',
     source => 'puppet:///modules/condor/91_single_host.config',
+    require => Package['condor']
+  }
+  file {'/etc/condor/config.d/92_uclhc_flock_to.config':
+    ensure => 'present',
+    source => 'puppet:///modules/condor/92_uclhc_flock_to.config',
     require => Package['condor']
   }
 
@@ -22,6 +37,12 @@ class condor ($condor_version = 'latest', $gwms_version = 'latest') {
     enable => 'true',
     ensure => 'running',
     require => Package['condor'],
-    subscribe => [Package['glideinwms-userschedd'], File['/etc/condor/config.d/91_single_host.config']]
+    subscribe => [
+      Package['glideinwms-userschedd'],
+      File['/etc/condor/config.d/91_single_host.config',
+        '/etc/condor/config.d/92_uclhc_flock_to.config',
+        '/etc/condor/config.d/90_gwms_dns.config',
+        '/etc/condor/certs/condor_mapfile'
+      ]]
   }
 }
