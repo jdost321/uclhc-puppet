e92fc9b73fa18a5138ca7ccd3b9e6a6615ffb79b
Edgar Fajardo <efajardo@physics.ucsd.edu>
2015-09-09 11:00:32 -0700

    Added the svn and git

--- client_tools/manifests/init.pp
+++ client_tools/manifests/init.pp
@@ -2,13 +2,25 @@ class client_tools {
   require osg_repos
 
   package { 'vo-client':
-    ensure => 'latest',
-    provider => 'yum',
+    ensure          => 'latest',
+    provider        => 'yum',
     install_options => [{'--enablerepo' => 'epel'}, {'--enablerepo' => 'osg'}]
   }
   package { 'voms-clients':
-    ensure => 'latest',
-    provider => 'yum',
+    ensure          => 'latest',
+    provider        => 'yum',
     install_options => [{'--enablerepo' => 'epel'}, {'--enablerepo' => 'osg'}]
+  }
+
+  package { 'git':
+    ensure          => 'present',
+    provider        => 'yum',
+    install_options => [{'--enablerepo' => 'epel'}]
+  }
+
+  package { 'subversion':
+    ensure          => 'present',
+    provider        => 'yum',
+    install_options => [{'--enablerepo' => 'epel'}]
   }
 }
