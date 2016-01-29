2ae5e07322b34be6d40228a431af0ec6fa1264f4
Jeff Dost <jdost@ucsd.edu>
2016-01-29 15:39:34 -0800

    allow for osg repo version selection and keep repos always enabled

--- osg_repos/manifests/init.pp
+++ osg_repos/manifests/init.pp
@@ -1,30 +1,19 @@
-class osg_repos {
+class osg_repos (
+    $release = '3.2'
+  ) {
   package { 'epel-release':
-    ensure => 'installed',
+    ensure => 'latest',
     provider => 'rpm',
-    source => 'http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm'
-  } ->
-  ini_setting { 'disable_epel':
-    path => '/etc/yum.repos.d/epel.repo',
-    section => 'epel',
-    setting => 'enabled',
-    value => '0'
+    source => "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$operatingsystemmajrelease.noarch.rpm"
   } ->
   package { 'yum-plugin-priorities':
     ensure => 'latest',
     provider => 'yum',
-    install_options => [{'--enablerepo' => 'epel'}]
   }
 
   package { 'osg-release':
-    ensure => 'installed',
+    ensure => 'latest',
     provider => 'rpm',
-    source => 'http://repo.grid.iu.edu/osg/3.2/osg-3.2-el6-release-latest.rpm'
-  } ->
-  ini_setting { 'disable_osg':
-    path => '/etc/yum.repos.d/osg-el6.repo',
-    section => 'osg',
-    setting => 'enabled',
-    value => '0'
+    source => "http://repo.grid.iu.edu/osg/$release/osg-$release-el$operatingsystemmajrelease-release-latest.rpm"
   }
 }
