679efc2d3d572a66d71bc698c98868701f055b03
Jeff Dost <jdost@ucsd.edu>
2016-03-28 09:46:28 -0700

    add some temporary mods to auth for edgar scale test

--- condor/manifests/auth_config.pp
+++ condor/manifests/auth_config.pp
@@ -1,8 +1,11 @@
 class condor::auth_config {
-  $mapfile = $condor::enable_condor_c ? {
+  /*$mapfile = $condor::enable_condor_c ? {
     true => 'condor_mapfile.condor_c.erb',
     false => 'condor_mapfile.erb'
   }
+  */
+
+  $mapfile = 'condor_mapfile.erb'
 
   file { '/etc/condor/certs/condor_mapfile':
     ensure => 'present',
