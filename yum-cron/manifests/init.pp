45c542bb8e3303f6b819c04c354b9064d43b34e1
Jeff Dost <jdost@ucsd.edu>
2016-06-16 16:13:12 -0700

    add missing yum cron file

--- yum-cron/manifests/init.pp
+++ yum-cron/manifests/init.pp
@@ -2,13 +2,24 @@ class yum-cron {
   package { 'yum-cron':
     ensure => 'latest',
     provider => 'yum'
-  } ->
+  }
+
   file { '/etc/sysconfig/yum-cron':
     ensure  => 'present',
     source  => 'puppet:///modules/yum-cron/yum-cron',
-  } ~>
+    require => Package['yum-cron'],
+    notify => Service['yum-cron']
+  }
+
+  file { '/etc/yum/yum-cron.conf':
+    ensure  => 'present',
+    source  => 'puppet:///modules/yum-cron/yum-cron.conf',
+    require => Package['yum-cron'],
+    notify => Service['yum-cron']
+  }
+
   service { 'yum-cron':
     enable => true,
-    ensure => running,
+    ensure => running
   }
 }
