512cf45f10ddc9399b57aed12ac815f0bf0e9099
Jeff Dost <jdost@ucsd.edu>
2016-10-27 08:52:10 -0700

    put esmond_client in its own class

--- prp_maddash/manifests/init.pp
+++ prp_maddash/manifests/init.pp
@@ -11,23 +11,7 @@ class prp_maddash {
     provider => 'yum'
   }
 
-  package { 'centos-release-scl':
-    ensure => 'latest',
-    provider => 'yum'
-  } ->
-  package { 'python27':
-    ensure => 'latest',
-    provider => 'yum'
-  } ->
-  file { '/root/esmond-gridftp-install.sh':
-    ensure => 'present',
-    source  => 'puppet:///modules/prp_maddash/esmond-gridftp-install.sh',
-    mode => '0755'
-  } ->
-  exec { '/root/esmond-gridftp-install.sh':
-    creates => '/opt/esmond-gridftp/bin/esmond-ps-load-gridftp'
-  }
-
+  include prp_maddash::esmond_client
   include prp_maddash::gridftp_config
 
   service { 'globus-gridftp-server':
