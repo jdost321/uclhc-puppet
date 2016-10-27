30ebf9dff67afb1067396435adaed7c0d7032c30
Jeff Dost <jdost@ucsd.edu>
2016-10-27 06:14:19 -0700

    add installation of esmond-client into virtualenv

--- prp_maddash/manifests/init.pp
+++ prp_maddash/manifests/init.pp
@@ -18,6 +18,14 @@ class prp_maddash {
   package { 'python27':
     ensure => 'latest',
     provider => 'yum'
+  } ->
+  file { '/root/esmond-gridftp-install.sh':
+    ensure => 'present',
+    source  => 'puppet:///modules/prp_maddash/esmond-gridftp-install.sh',
+    mode => '0755'
+  } ->
+  exec { '/root/esmond-gridftp-install.sh':
+    creates => '/opt/esmond-gridftp/bin/esmond-ps-load-gridftp'
   }
 
   include prp_maddash::gridftp_config
