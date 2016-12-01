b85df20f5be0f3c3765d36e95e493fd0ed7449d9
Jeff Dost <jdost@ucsd.edu>
2016-12-01 15:51:41 -0800

    fix typo in path

--- prp_maddash/files/cron-load-gridftp.sh
+++ prp_maddash/files/cron-load-gridftp.sh
@@ -7,7 +7,7 @@ fi
 
 scl enable python27 - << EOF
 source /opt/esmond-gridftp/bin/activate
-python /usr/bin/esmond-ps-load-gridftp -p /opt/esmond-gridftp/prp-maddash.calit2.optiputer.net.pickle -U http://prp-maddash.calit2.optiputer.net/esmond -u gridftp -k 93aacc45ecb4fc816c71022f3dfe81a65e96b60a -f /var/log/gridftp-transfer.log
+python /opt/esmond-gridftp/bin/esmond-ps-load-gridftp -p /opt/esmond-gridftp/prp-maddash.calit2.optiputer.net.pickle -U http://prp-maddash.calit2.optiputer.net/esmond -u gridftp -k 93aacc45ecb4fc816c71022f3dfe81a65e96b60a -f /var/log/gridftp-transfer.log
 deactivate
 EOF
 
