6736b862d9535dd6e8044453d31502e636defb4a
root <root@uclhcpup.t2.ucsd.edu>
2017-04-07 08:56:21 -0700

    add gwms factory package

--- gwms_factory/manifests/factory.pp
+++ gwms_factory/manifests/factory.pp
@@ -7,4 +7,10 @@ class gwms_factory::factory (
   include gwms_factory::factory::osg_repos
   include gwms_factory::factory::grid_certs
   include gwms_factory::factory::condor
+
+  package { 'glideinwms-factory':
+    ensure => $gwms_factory::factory::gwms_version,
+    provider => 'yum',
+    require => Class['gwms_factory::factory::condor']
+  }
 }
