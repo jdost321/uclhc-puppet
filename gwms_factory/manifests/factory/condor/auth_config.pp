7a33bf7bac78342475c823d856960cadea60bb4a
Jeff Dost <jdost@ucsd.edu>
2017-04-06 17:23:09 -0700

    add privsep config

--- gwms_factory/manifests/factory/condor/auth_config.pp
+++ gwms_factory/manifests/factory/condor/auth_config.pp
@@ -5,4 +5,8 @@ class gwms_factory::factory::condor::auth_config {
     ensure => 'present',
     content => template('gwms_factory/factory/condor/condor_mapfile.erb'),
   }
+  file { '/etc/condor/privsep_config':
+    ensure => 'present',
+    content => template('gwms_factory/factory/condor/privsep_config.erb'),
+  }
 }
