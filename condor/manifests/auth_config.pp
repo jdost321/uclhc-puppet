31dd937d923167601c36de710a9f1fc10e789b16
Jeff Dost <jdost@ucsd.edu>
2017-04-25 14:02:14 -0700

    close some security holes

--- condor/manifests/auth_config.pp
+++ condor/manifests/auth_config.pp
@@ -1,11 +1,8 @@
 class condor::auth_config {
-  /*$mapfile = $condor::enable_condor_c ? {
+  $mapfile = $condor::enable_condor_c ? {
     true => 'condor_mapfile.condor_c.erb',
     false => 'condor_mapfile.erb'
   }
-  */
-
-  $mapfile = 'condor_mapfile.erb'
 
   file { '/etc/condor/certs/condor_mapfile':
     ensure => 'present',
